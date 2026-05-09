b19ead02d7cdb8faec05c039139a0d10fafcc795
FabianorellanaG <fabian.ignacio.og@gmail.com>
2026-05-09 16:18:26 -0400

    Update theme configuration by removing email links
    Removed email links and updated configuration settings.

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,11 +1,32 @@
-// theme.config.jsx
 export default {
   logo: <span>CFTSA Office 365</span>,
+
   project: {
-    // link: 'ailto:fabian.orellana@cftsanagustin.cl' // optional
+    link: ''
   },
-  docsRepositoryBase: 'mailto:fabian.orellana@cftsanagustin.cl',
+
+  docsRepositoryBase: '',
+
   footer: {
     text: 'CFTSA Office 365 Guide'
+  },
+
+  sidebar: {
+    toggleButton: false,
+    defaultMenuCollapseLevel: 10
+  },
+
+  toc: {
+    float: false
+  },
+
+  navigation: false,
+
+  feedback: {
+    content: null
+  },
+
+  editLink: {
+    component: null
   }
 }
